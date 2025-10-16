0353af0b63077aad44464632637e4546ba15f1d0
Philipp Baasch <philipp.baasch@gmail.com>
2025-10-16 18:16:11 +0200

    minor fix

--- src/mesh/visualization.jl
+++ src/mesh/visualization.jl
@@ -128,7 +128,7 @@ function MeshData_to_vtk(md::MeshData, rd::RefElemData, data, dataname, filename
 
     if equi_dist_nodes == true
         coords = map(x -> vec(rd.Vp * x), md.xyz)
-        data_interpolated = Vector{Matrix{eltype(data[1])}}(undef, length(data))
+        data_interpolated = Vector{Matrix{eltype(eltype(data))}}(undef, length(data))
         for (i, data_i) in enumerate(data)
             data_interpolated[i] = rd.Vp * data_i
         end
