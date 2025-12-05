b748cae1d6038ec2bd2f72e4fad114fd5b04018b
Philipp Baasch <philipp.baasch@gmail.com>
2025-12-05 10:16:27 +0100

    2D mesh in 3D

--- src/geometric_functions.jl
+++ src/geometric_functions.jl
@@ -1,6 +1,6 @@
 """
     geometric_factors(x, y, Dr, Ds)
-    geometric_factors(x, y, z, Dr, Ds, Dt, Filters=(I, I, I))
+    geometric_factors(x, y, z, Dr, Ds, Dt; Filters=(I, I, I))
 
 Compute metrics of mappings between "real" elements and reference elements,
 outward pointing normals on faces of every elements, and Jacobian.
@@ -25,7 +25,7 @@ function geometric_factors(x, y, Dr, Ds)
     return rxJ, sxJ, ryJ, syJ, J
 end
 
-function geometric_factors(x, y, z, Dr, Ds, Dt, Filters=(I, I, I))
+function geometric_factors(x, y, z, Dr, Ds, Dt; Filters=(I, I, I))
 
     xr, xs, xt = Dr * x, Ds * x, Dt * x
     yr, ys, yt = Dr * y, Ds * y, Dt * y
@@ -54,12 +54,34 @@ function geometric_factors(x, y, z, Dr, Ds, Dt, Filters=(I, I, I))
     return rxJ, sxJ, txJ, ryJ, syJ, tyJ, rzJ, szJ, tzJ, J
 end
 
+function geometric_factors(x, y, z, Dr, Ds; Filters=(I, I, I))
+
+    xr, xs = Dr * x, Ds * x
+    yr, ys = Dr * y, Ds * y
+    zr, zs = Dr * z, Ds * z
+
+    a1 = Dr * x, Dr * y, Dr * z
+    a2 = Ds * x, Dr * y, Ds * z
+    
+    a3 = @. a1[2] * a2[3] - a1[3] * a2[2], a1[3] * a2[1] - a1[1] * a2[3], a1[1] * a2[2] - a1[2] * a2[1]
+    norma3 = sqrt.(a3[1].^2 + a3[2].^2 + a3[3].^2)
+    a3 = @. a3[1] / norma3, a3[2] / norma3, a3[3] / norma3
+
+    rxJ, ryJ, rzJ = @. a2[2] * a3[3] - a2[3] * a3[2], a2[3] * a3[1] - a2[1] * a3[3], a2[1] * a3[2] - a2[2] * a3[1]
+    sxJ, syJ, szJ = @. a3[2] * a1[3] - a3[3] * a1[2], a3[3] * a1[1] - a3[1] * a1[3], a3[1] * a1[2] - a3[2] * a1[1]
+
+    J = @. sqrt((xr^2 + yr^2 + zr^2) * (xs^2 + ys^2 + zs^2) - (xr * xs + yr * ys + zr * zs)^2)
+
+    return rxJ .* 0, sxJ .* 0, ryJ .* 0, syJ, rzJ, szJ, J
+end
+
 # physical outward normals are computed via Nanson's formula: G * nhatJ, where 
 # G = matrix of J-scaled geometric terms. Here, Vf is a face interpolation matrix 
 # which maps interpolation nodes to face nodes. 
-function compute_normals(geo::SMatrix{Dim, Dim}, Vf, nrstJ...) where {Dim}
-    nxyzJ = ntuple(x -> zeros(size(Vf, 1), size(first(geo), 2)), Dim)
-    for i = 1:Dim, j = 1:Dim
+function compute_normals(geo::SMatrix{Dim, DimAmbient}, Vf, nrstJ...) where {Dim, DimAmbient}
+    nxyzJ = ntuple(x -> zeros(size(Vf, 1), size(first(geo), 2)), DimAmbient)
+    for i = 1:Dim, j = 1:DimAmbient
+        @show geo[i, j]
         nxyzJ[i] .+= (Vf * geo[i,j]) .* nrstJ[j]
     end
     Jf = sqrt.(sum(map(x -> x.^2, nxyzJ)))
